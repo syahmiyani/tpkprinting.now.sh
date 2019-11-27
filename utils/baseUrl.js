65c01c57ac4acf8f273c38df4b55e377ebc269c4
syahmiyani <syahmiahmad461@gmail.com>
2019-11-27 16:08:10 +0800

    point prod baseUrl to now.sh

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://deployment-url.now.sh"
+    ? "https://tpkprinting.now.sh"
     : "http://localhost:3000";
 
 export default baseUrl;
